3a4ffc21f8a9656bedd8544862f4d8e85083698e
CPearl0 <1269596929@qq.com>
2024-08-12 10:37:55 +0800

    modifications

--- kubejs/server_scripts/src/mod/daybreak_core.js
+++ kubejs/server_scripts/src/mod/daybreak_core.js
@@ -1,3 +1,14 @@
 ServerEvents.recipes(event => {
-
+    event.shaped("daybreakcore:sun_rocket", [
+        "CNC",
+        "SAS",
+        "RER"
+    ], {
+        C: "powah:crystal_nitro",
+        N: "#c:nether_stars",
+        S: "ae2:singularity",
+        A: "mekanism:pellet_antimatter",
+        R: "minecraft:firework_rocket",
+        E: "minecraft:end_rod",
+    });
 });
--- kubejs/server_scripts/src/mod/mek.js
+++ kubejs/server_scripts/src/mod/mek.js
@@ -4,6 +4,7 @@ ServerEvents.recipes(event => {
         "mekanism:steel_casing",
         "mekanism:metallurgic_infuser",
         "mekanism:energized_smelter",
+        "mekanismgenerators:wind_generator",
     ]);
 
     event.shaped("mekanism:steel_casing", [
@@ -35,5 +36,16 @@ ServerEvents.recipes(event => {
         E: "powah:steel_energized",
         C: "powah:capacitor_hardened",
         S: "mekanism:steel_casing",
+    });
+    event.shaped("mekanismgenerators:wind_generator", [
+        "OOO",
+        "ACA",
+        "EDE",
+    ], {
+        O: "#c:ingots/osmium",
+        A: "#c:alloys/advanced",
+        C: "powah:dielectric_casing",
+        E: "mekanism:energy_tablet",
+        D: "mekanism:advanced_control_circuit",
     });
 });
